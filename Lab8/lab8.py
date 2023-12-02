19623b8e420993950294568ad6eecaf65a9f1134
EoinConcannon <101334026+EoinConcannon@users.noreply.github.com>
2023-12-02 19:50:06 +0000

    L8 working on displaying image

--- Lab8/lab8.py
+++ Lab8/lab8.py
@@ -3,8 +3,9 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('ATU.jpg',)
+img = cv2.imread('ATU.jpg')
 
-print("hello")
+cv2.imshow('image', img)
+cv2.waitKey()
 
 # https://stackoverflow.com/questions/55686826/how-can-i-properly-convert-image-to-gray-scale-in-opencv
