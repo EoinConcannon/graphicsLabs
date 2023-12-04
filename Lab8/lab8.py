96bf4c65adadbf0a5dd0e2d0090077dd1fa5d071
EoinConcannon <101334026+EoinConcannon@users.noreply.github.com>
2023-12-04 09:43:02 +0000

    L8 converts image to greyscale

--- Lab8/lab8.py
+++ Lab8/lab8.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('ATU.jpg')
 
+# https://stackoverflow.com/questions/55686826/how-can-i-properly-convert-image-to-gray-scale-in-opencv
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # converts to greyscale
 cv2.imshow('image', img)
 cv2.waitKey()
-
-# https://stackoverflow.com/questions/55686826/how-can-i-properly-convert-image-to-gray-scale-in-opencv
