e94ce693cf7dd7a6aedc8aaeb0fcd2b1bcc0dd31
EoinConcannon <101334026+EoinConcannon@users.noreply.github.com>
2023-12-05 11:36:03 +0000

    L8 four imgs displayed, two new added imgs display blur

--- Lab8/lab8.py
+++ Lab8/lab8.py
@@ -11,10 +11,27 @@ img = cv2.imread('ATU.jpg')
 # cv2.waitKey()
 
 nrows = 2
-ncols = 1
+ncols = 2
 
 plt.subplot(nrows, ncols,1),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
 plt.subplot(nrows, ncols,2),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), cmap = 'gray')
 plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
+
+KernelSizeWidth = 3
+KernelSizeHeight = 3
+
+img = cv2.GaussianBlur(img,(KernelSizeWidth, KernelSizeHeight),0)
+
+
+plt.subplot(nrows, ncols,3),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), cmap = 'gray')
+plt.title('3x3 Blur'), plt.xticks([]), plt.yticks([])
+
+KernelSizeWidth = 13
+KernelSizeHeight = 13
+
+img = cv2.GaussianBlur(img,(KernelSizeWidth, KernelSizeHeight),0)
+
+plt.subplot(nrows, ncols,4),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), cmap = 'gray')
+plt.title('13x13 Blur'), plt.xticks([]), plt.yticks([])
 plt.show()
