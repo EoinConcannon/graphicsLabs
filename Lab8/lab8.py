0aa20921b0966bca766a5724aa09132ce4a5650f
EoinConcannon <101334026+EoinConcannon@users.noreply.github.com>
2023-12-05 11:25:50 +0000

    L8 displays original and greyscale imgs

--- Lab8/lab8.py
+++ Lab8/lab8.py
@@ -15,6 +15,6 @@ ncols = 1
 
 plt.subplot(nrows, ncols,1),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
-plt.subplot(nrows, ncols,2),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), cmap = 'gray')
+plt.subplot(nrows, ncols,2),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), cmap = 'gray')
 plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
 plt.show()
