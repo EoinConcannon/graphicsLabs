3b8e7dfaa76328e826fe9051181dfe39b67676d7
EoinConcannon <101334026+EoinConcannon@users.noreply.github.com>
2023-12-05 12:20:58 +0000

    L8 added img that combines previous sobel imgs, changed code to stop converting to greyscale, more img variables added

--- Lab8/lab8.py
+++ Lab8/lab8.py
@@ -4,38 +4,46 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img = cv2.imread('ATU.jpg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # prevent colour issues
 
 nrows = 2
-ncols = 2
+ncols = 3
+
+greyImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # converts to greyscale
 
 # no blur original & greyscale
-plt.subplot(nrows, ncols,1),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), cmap = 'gray')
+plt.subplot(nrows, ncols,1),plt.imshow(img, cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
-plt.subplot(nrows, ncols,2),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), cmap = 'gray')
+plt.subplot(nrows, ncols,2),plt.imshow(greyImg, cmap = 'gray')
 plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
 
 # 3x3 blur set
 KernelSizeWidth = 3
 KernelSizeHeight = 3
-img = cv2.GaussianBlur(img,(KernelSizeWidth, KernelSizeHeight),0)
-
+imgBlur = cv2.GaussianBlur(greyImg,(KernelSizeWidth, KernelSizeHeight),0)
 # sobel edge detection vertical
-sobelVertical = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5) # y dir
+# https://stackoverflow.com/questions/51167768/sobel-edge-detection-using-opencv
+sobelVertical = cv2.Sobel(imgBlur,cv2.CV_64F,0,1,ksize=5) # y dir
 verticalImg = cv2.convertScaleAbs(sobelVertical)
-
-plt.subplot(nrows, ncols,3),plt.imshow(cv2.cvtColor(verticalImg, cv2.COLOR_BGR2GRAY), cmap = 'gray')
-plt.title('3x3 Blur'), plt.xticks([]), plt.yticks([])
+# end result img
+plt.subplot(nrows, ncols,3),plt.imshow(verticalImg, cmap = 'gray')
+plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
 
 # 13x13 blur set
 KernelSizeWidth = 13
 KernelSizeHeight = 13
-img = cv2.GaussianBlur(img,(KernelSizeWidth, KernelSizeHeight),0)
-
+imgBlurStrong = cv2.GaussianBlur(greyImg,(KernelSizeWidth, KernelSizeHeight),0)
 # sobel edge detection horizontal
-sobelHorizontal = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5) # x dir
+sobelHorizontal = cv2.Sobel(imgBlurStrong,cv2.CV_64F,1,0,ksize=5) # x dir
 horizontalImg = cv2.convertScaleAbs(sobelHorizontal)
+# end result img
+plt.subplot(nrows, ncols,4),plt.imshow(horizontalImg, cmap = 'gray')
+plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(nrows, ncols,4),plt.imshow(cv2.cvtColor(horizontalImg, cv2.COLOR_BGR2GRAY), cmap = 'gray')
-plt.title('13x13 Blur'), plt.xticks([]), plt.yticks([])
+# combines previous x/y sobel imgs
+sobelCombined = cv2.addWeighted(verticalImg, 0.5, horizontalImg, 0.5, 0)
+
+# both sobel x/y imgs combined
+plt.subplot(nrows, ncols,5),plt.imshow(sobelCombined, cmap = 'gray')
+plt.title('Sobel Sum'), plt.xticks([]), plt.yticks([])
 plt.show()
-
