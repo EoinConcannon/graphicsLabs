1d958d7dbd7a31495a6d14154d3cd4e80bc7682d
EoinConcannon <G00406729@atu.ie>
2023-12-06 13:42:01 +0000

    L9 loaded img and grey img using opencv

--- Lab9/lab9.py
+++ Lab9/lab9.py
@@ -3,3 +3,10 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+img = cv2.imread('ATU1.jpg')
+# img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # prevent colour issues
+
+greyImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # converts to greyscale
+
+cv2.imshow('image', img)
+cv2.waitKey()
