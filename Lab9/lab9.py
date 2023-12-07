0e714b294e4ccb5c69dd3d5ac0a1a63500e84091
EoinConcannon <101334026+EoinConcannon@users.noreply.github.com>
2023-12-07 13:31:06 +0000

    L9 added Harris corner detection

--- Lab9/lab9.py
+++ Lab9/lab9.py
@@ -8,5 +8,21 @@ img = cv2.imread('ATU1.jpg')
 
 greyImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # converts to greyscale
 
-cv2.imshow('image', img)
+# Harris corner detection
+blockSize = 2
+aperture_size = 3
+k = 0.04
+
+dst = cv2.cornerHarris(greyImg, blockSize, aperture_size, k)
+
+# https://stackoverflow.com/questions/16533078/clone-an-image-in-cv2-python
+imgHarris = img.copy() # deep copy
+
+threshold = 0.1; #number between 0 and 1
+for i in range(len(dst)):
+    for j in range(len(dst[i])):
+        if dst[i][j] > (threshold*dst.max()):
+            cv2.circle(imgHarris,(j,i),3,(256, 0, 256),-1)
+
+cv2.imshow('image', imgHarris)
 cv2.waitKey()
